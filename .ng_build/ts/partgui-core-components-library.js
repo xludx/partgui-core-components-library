040bb5aceba9718f02c70347f40998472410cf9e
Juha Kovanen <juha.kovanen@gmail.com>
2017-09-19 17:44:19 +0300

    ignored and removed .ng_build

--- .ng_build/ts/partgui-core-components-library.js
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export { ExampleModule } from './public_api';
-export { ExampleComponent as ɵa } from './src/app/modules/example/example.component';
-//# sourceMappingURL=partgui-core-components-library.js.map
--- .ng_build/ts/public_api.js
+++
@@ -1,2 +0,0 @@
-export { ExampleModule } from './src/app/modules/example/example.module';
-//# sourceMappingURL=public_api.js.map
